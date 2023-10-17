4da2837f0e0ee700558b671d34cb009adf0807eb
satse13 <atobiasangeles@gmail.com>
2023-10-17 21:01:42 +0200

    Endpoint para dos preguntas por letra funcional

--- controllers/questions.js
+++ controllers/questions.js
@@ -3,8 +3,26 @@ const Question = require('../models/question')
 
 questionRouter.get('/', async (request, response) => {
 	const questions = await Question.find({})
-
 	response.json(questions)
+})
+
+questionRouter.get('/local',async (request, response) => {
+	const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+	const result = {}
+
+	for (const letter of letters) {
+		// Realiza una consulta para obtener dos preguntas aleatorias con la letra actual
+		const questions = await Question.aggregate([
+			{ $match: { letter } },
+			{ $sample: { size: 2 } }
+		])
+
+		// Agrega las preguntas aleatorias al objeto de resultado
+		result[letter] = questions
+	}
+
+	response.json(result)
+
 })
 
 module.exports = questionRouter
