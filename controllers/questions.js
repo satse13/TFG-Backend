219fb4e2c9020e523e1afa1e51195bdb64321709
ikercb <ikercuenca02@gmail.com>
2024-03-09 11:47:42 +0100

    Preparando el terreno para generar famosos para temas en los cuales no pueda sacar una palabra relacionada

--- controllers/questions.js
+++ controllers/questions.js
@@ -46,39 +46,20 @@ questionRouter.get('/solo',async (request, response) => {
 
 questionRouter.get('/generate', async (request, response) => {
 	const question = await generateQuestion(request.query.letter, request.query.theme, request.query.example)
-	response.json(question)
-})
-
-questionRouter.post('/questions', async (request, response) => {
-	const questions = request.body
-	let added = []
-
-	for(const key in questions){
-		const repeatedQuestion = await Question.findOne({word: questions[key].word})
-		if(repeatedQuestion == null){
-            const newQuestion = new Question({
-                letter: questions[key].letter,
-                word: questions[key].word,
-                description: questions[key].description,
-                startsWith: questions[key].startsWith,
-                Idioma: 'Eng'
-            });
-            newQuestion.save()
-            .then(() => {
-                console.log('Elemento añadido a la base de datos');
-            })
-            .catch((err) => {
-                console.error('Error al añadir elemento:', err);
-            });
-			added.push('Y')
-		}
-		else{
-			console.log('Elemento repetido y no añadido')
-			added.push('N')
-		}
+	const repeatedQuestion = await Question.findOne({word: question.word})
+	if(repeatedQuestion == null){
+		question.save()
+		.then(() => {
+			console.log('Elemento añadido a la base de datos');
+		})
+		.catch((err) => {
+			console.error('Error al añadir elemento:', err);
+		});
 	}
-	
-	response.json(added)
+	else{
+		console.log('Elemento repetido y no añadido')
+	}
+	response.json(question)
 })
 
 export default questionRouter
--- services/ChatGPT_API.js
+++ services/ChatGPT_API.js
@@ -3,7 +3,7 @@ import OpenAI from 'openai'
 import axios from 'axios'
 
 const openai = new OpenAI({
-	apiKey: process.env.API_KEY // This is also the default, can be omitted
+	apiKey: process.env.API_KEY
 })
 
 //Función para llamar a la API
@@ -28,10 +28,8 @@ const llamadaAPI = async (promptUser, promptAssistant, tokens, temperatura, mode
 	}
 }
 
-
-
 //Función para buscar palabras que sean válidas
-const getResponse = async (letra, promptPedirPalabra, ejemploSalidaPalabra) => {
+const getWord = async (letra, promptPedirPalabra, ejemploSalidaPalabra) => {
 	//Variables
 	let palValida = 'N'
 	let empiezaCon = ''
@@ -92,26 +90,15 @@ const getResponse = async (letra, promptPedirPalabra, ejemploSalidaPalabra) => {
 	}
 }
 
-function borrarHastaPalabra(str, palabra) {
-	const strMinuscula = str.toLowerCase()
-	const palabraMinuscula = palabra.toLowerCase()
-	const indice = strMinuscula.indexOf(palabraMinuscula)
-	if (indice !== -1) {
-		return str.substring(indice+1 + palabra.length)
-	} else {
-		return str
-	}
-}
-
 //Función para buscar definiciones que sean válidas
-const getDesc = async (palabra, promptPedirDesc, ejemploSalidaDesc, descripcion, theme) => {
+const getDesc = async (palabra, promptPedirDesc, ejemploSalidaDesc, theme) => {
 	//Variables
+	let descripcion
 	let descValida = 'N'
 	let bucle = 0
 
 	//Bucle para pedir definiciones hasta que sea válida
 	while(descValida == 'N' && bucle < 3){
-
 		//Pedir definición
 		const desc = await llamadaAPI(promptPedirDesc, ejemploSalidaDesc, 100, 0.5, 'gpt-3.5-turbo')
 
@@ -146,7 +133,6 @@ const getDesc = async (palabra, promptPedirDesc, ejemploSalidaDesc, descripcion,
 		}
 		else{
 			promptComprobarDefinición= 'Now: Answer yes or no:\nWord: ' + palabra + '\nDescription: ' + descripcion + '\nDoes the description accurately represent the provided word? Is the definition related with ' + theme + '?'
-			console.log("IN")
 		}
 		const promptEjemploComprobarDefinición = 'Sí.'
 
@@ -165,7 +151,167 @@ const getDesc = async (palabra, promptPedirDesc, ejemploSalidaDesc, descripcion,
 			descValida = 'N'
 			bucle++
 		}
+	} 
 
+	return {
+		desc: descripcion,
+		valida: descValida
+	}
+}
+
+const borrarHastaPalabra = (str, palabra) => {
+	const strMinuscula = str.toLowerCase()
+	const palabraMinuscula = palabra.toLowerCase()
+	const indice = strMinuscula.indexOf(palabraMinuscula)
+	if (indice !== -1) {
+		return str.substring(indice+1 + palabra.length)
+	} else {
+		return str
+	}
+}
+
+//Función para buscar palabras que sean válidas
+const getFamous = async (letra, promptPedirFamoso, ejemploSalidaPalabra) => {
+	//Variables
+	let valido = 'N'
+	let empiezaCon = ''
+	let nombre = ''
+	let apellido;
+
+	//Pedir la palabra
+	let famous = await llamadaAPI(promptPedirFamoso, ejemploSalidaPalabra, 10, 1.1, 'gpt-3.5-turbo')
+    famous = famous.trim().split(' ')
+	nombre = famous[0]
+	apellido = famous[1]
+	
+	console.log(nombre + ' ' + apellido)
+
+	//********************************************//
+	//Vemos si existe en Wikipedia
+	palabra = palabra.trim()
+	const apiUrl = 'https://en.wikipedia.org/w/rest.php/v1/search/page?q=' + nombre + '_' + apellido
+	const respuesta  = await axios.get(apiUrl)
+
+	if(!respuesta.data.pages.length){
+		console.log('NO aparece en wikipedia')
+		return {
+			word: palabra,
+			empieza: empiezaCon,
+			valida: valido
+		}
+	}
+	else{
+		console.log('SI aparece en wikipedia')
+	}
+    
+	//********************************************//
+	//Si no contiene la letra, error y salir
+	if(nombre.includes(letra)){
+		valido = 'Y'
+		if(nombre.startsWith(letra)){
+			empiezaCon = 'Y'
+			console.log('Empieza por ' + letra)
+		}
+		else{
+			empiezaCon = 'N'
+			console.log('No empieza por ' + letra)
+		}
+	
+		return {
+			word: nombre,
+			empieza: empiezaCon,
+			valida: valido
+		}
+	}
+	
+	if(apellido.includes(letra)){
+		valido = 'Y'
+		if(apellido.startsWith(letra)){
+			empiezaCon = 'Y'
+			console.log('Empieza por ' + letra)
+		}
+		else{
+			empiezaCon = 'N'
+			console.log('No empieza por ' + letra)
+		}
+	
+		return {
+			word: apellido,
+			empieza: empiezaCon,
+			valida: valido
+		}
+	}
+
+	console.log('Error: Ni nombre ni apellido contienen la letra '+ letra)
+        
+	return {
+		word: nombre + ' ' + apellido,
+		empieza: empiezaCon,
+		valida: valido
+	}
+}
+
+//Función para buscar definiciones que sean válidas
+const getInfo = async (palabra, promptPedirInfo, ejemploSalidaInfo, theme) => {
+	//Variables
+	let descripcion
+	let descValida = 'N'
+	let bucle = 0
+
+	//Bucle para pedir definiciones hasta que sea válida
+	while(descValida == 'N' && bucle < 3){
+		//Pedir definición
+		const desc = await llamadaAPI(promptPedirInfo, ejemploSalidaInfo, 100, 0.5, 'gpt-3.5-turbo')
+
+		descripcion = desc.contenido
+        
+		console.log(descripcion)
+
+		descripcion = borrarHastaPalabra(descripcion,palabra)
+
+		descripcion = descripcion.trim()
+
+		const caracter = descripcion.charAt(0).toUpperCase()
+
+		descripcion = caracter + descripcion.substring(1)
+
+		const descMinus = descripcion.toLowerCase()
+
+		console.log(descripcion)
+
+		//Si la definición contiene la palabra, error y volver a pedir
+		if(descMinus.includes(palabra)){
+			console.log('Error: La definición contiene la palabra ' + palabra)
+			bucle++
+			//Aqui puedo cambiar el prompt para que me de una definición sin la palabra
+			continue
+		}
+
+		//Si la definición no contiene la palabra, comprobar si es válida
+		let promptComprobarDefinición
+		if(theme == ''){
+			promptComprobarDefinición= 'Now: Answer yes or no:\nWord: ' + palabra + '\nDescription: ' + descripcion + '\nDoes the description accurately represent the provided word?'
+		}
+		else{
+			promptComprobarDefinición= 'Now: Answer yes or no:\nWord: ' + palabra + '\nDescription: ' + descripcion + '\nDoes the description accurately represent the provided word? Is the definition related with ' + theme + '?'
+		}
+		const promptEjemploComprobarDefinición = 'Sí.'
+
+		const comprobarDefinicion = await llamadaAPI(promptComprobarDefinición, promptEjemploComprobarDefinición, 2, 0.5, 'gpt-3.5-turbo')
+
+		console.log(comprobarDefinicion.contenido)
+
+		//Si la definición es válida, descValida = 'Y', si no, descValida = 'N'
+		const arraySoluciones = ['Sí', 'Si.', 'Yes.', 'Yes']
+
+		if(arraySoluciones.indexOf(comprobarDefinicion.contenido) !== -1){ 
+			descValida = 'Y'
+			console.log('Palabra válida')
+		}
+		else{
+			descValida = 'N'
+			bucle++
+		}
 	} 
 
 	return {
@@ -176,30 +322,48 @@ const getDesc = async (palabra, promptPedirDesc, ejemploSalidaDesc, descripcion,
 
 async function generateQuestion(letter, theme, example) {
 	let newQuestion
-	// eslint-disable-next-line no-constant-condition
+	let iterations = 0
 	while(true){
-		const promptPedirPregunta = 'Generate only, without any unnecessary message and without punctuation, a word that exists in English, that starts with the letter '+ letter +' that is common and has to do with ' + theme + '.'
-		const ejemploSalidaPregunta = example
-		const descripcion = ''
+		if(iterations < 3) {
+			const promptPedirPregunta = 'Generate only, without any unnecessary message and without punctuation, a word that exists in English, that starts with the letter '+ letter +' that is common and has to do with ' + theme + '.'
+			const palStruct = await getWord(letter, promptPedirPregunta, example)
+			if (palStruct.valida == 'N') continue
 
-		const promptPal = await getResponse(letter, promptPedirPregunta, ejemploSalidaPregunta)
-		if (promptPal.valida == 'N') continue
-		const promptPedirDesc = 'Generate a definition of the word '+ promptPal.word +' in 20 words. Include essential information about its meaning and common use.'
-		const ejemploSalidaDesc = ''
-            
-		const promptDef = await getDesc(promptPal.word,promptPedirDesc, ejemploSalidaDesc, descripcion, theme)
-		if (promptDef.valida == 'N') continue    
+			const promptPedirDesc = 'Generate a definition of the word '+ palStruct.word +' in 20 words. Include essential information about its meaning and common use.'
+			const defStruct = await getDesc(palStruct.word,promptPedirDesc, '', theme)
+			if (defStruct.valida == 'N') continue    
 
-		// Crea una instancia del modelo y guarda en la base de datos
-		newQuestion = new Question({
-			letter: letter,
-			word: promptPal.word,
-			description: promptDef.desc,
-			startsWith: promptPal.empieza,
-			Idioma: 'Eng'
-		})
+			// Crea una instancia del modelo y guarda en la base de datos
+			newQuestion = new Question({
+				letter: letter,
+				word: palStruct.word,
+				description: defStruct.desc,
+				startsWith: palStruct.empieza,
+				Idioma: 'Eng'
+			})
 
-		return newQuestion
+			return newQuestion
+		}
+		else{
+			const promptPedirFamoso = 'Give me the name and surname of a famous individual whose career is related with ' + theme + '. The name or surname must start with ' + letter + '. Give only this two words.'
+			const famousStruct = await getFamous(letter, promptPedirFamoso, '')
+			if (amousStruct.valida == 'N') continue
+
+			const promptPedirInfo = ''
+			const infoStruct = {}// = await getInfo(promptPal.word,promptPedirDesc, '', descripcion, theme)
+			if (infoStruc.valida == 'N') continue    
+
+			// Crea una instancia del modelo y guarda en la base de datos
+			newQuestion = new Question({
+				letter: letter,
+				word: famousStruct.word,
+				description: infoStruc.desc,
+				startsWith: famousStruct.empieza,
+				Idioma: 'Eng'
+			})
+
+			return newQuestion
+		}
 	}
 }
 
